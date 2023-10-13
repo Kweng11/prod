1f085101ee27e7efe4e46e52660fb2bfe0e78e05
Stipen0 <charlesestimada25@gmail.com>
2023-10-13 22:41:05 +0800

    Update Hero.vue

--- src/Kira/Hero.vue
+++ src/Kira/Hero.vue
@@ -1,73 +1,73 @@
 <template>
-							<!-- menu start -->
-							<nav class="main-menu">
-								<ul>
-									<li class="current-list-item"><a href="/">Home</a>
+	<!-- menu start -->
+	<nav class="main-menu">
+		<ul>
+			<li class="current-list-item"><a href="/">Home</a>
 
-									</li>
-									<li><a href="shop.html">Shop</a></li>
-									<li><a href="about.html">About</a></li>
-                                     <li><a href="news.html">News</a>
-									</li>
-									<li><a href="contact.html">Contact</a>
+			</li>
+			<li><a href="shop.html">Shop</a></li>
+			<li><a href="about.html">About</a></li>
+			 <li><a href="news.html">News</a>
+			</li>
+			<li><a href="contact.html">Contact</a>
 
-									</li>
+			</li>
 
-									<li>
-										<div class="header-icons">
-											<a class="shopping-cart" href="cart.html"><i class="fas fa-shopping-cart"></i></a>
-											<a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
-											<a href="/logout">
-											<img src="" alt="">Logout</a>
-										</div>
-									</li>
-								</ul>
-							</nav>
-							<a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
-							<div class="mobile-menu"></div>
-							<!-- menu end -->
-						<!-- end header -->
-
-		<!-- search area -->
-		<div class="search-area">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<span class="close-btn"><i class="fas fa-window-close"></i></span>
-						<div class="search-bar">
-							<div class="search-bar-tablecell">
-								<h3>Search For:</h3>
-								<input type="text" placeholder="Keywords">
-								<button type="submit">Search <i class="fas fa-search"></i></button>
-							</div>
-						</div>
-					</div>
+			<li>
+				<div class="header-icons">
+					<a class="shopping-cart" href="cart.html"><i class="fas fa-shopping-cart"></i></a>
+					<a class="mobile-hide search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+					<a href="/logout">
+					<img src="" alt="">Logout</a>
 				</div>
+			</li>
+		</ul>
+	</nav>
+	<a class="mobile-show search-bar-icon" href="#"><i class="fas fa-search"></i></a>
+	<div class="mobile-menu"></div>
+	<!-- menu end -->
+<!-- end header -->
+
+<!-- search area -->
+<div class="search-area">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<span class="close-btn"><i class="fas fa-window-close"></i></span>
+<div class="search-bar">
+	<div class="search-bar-tablecell">
+		<h3>Search For:</h3>
+		<input type="text" placeholder="Keywords">
+		<button type="submit">Search <i class="fas fa-search"></i></button>
+	</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<!-- hero area -->
+		<div class="hero-area hero-bg">
+
+		  <div class="container">
+			<div class="row">
+			  <div class="col-lg-9 offset-lg-2 text-center">
+				<div class="hero-text">
+				  <div class="hero-text-tablecell">
+					  <img src="@/assets/kira/img/images/s.jpg" alt=""  width= 50%>
+					<p class="subtitle">Fresh & Organic</p>
+					<h1>Delicious Nuts</h1>
+					  <div class="hero-btns">
+					  <a href="shop.html" class="boxed-btn">Nuts Collection</a>
+					  <a href="contact.html" class="bordered-btn">Contact Us</a>
+
+					</div>
+				  </div>
+				</div>
+			  </div>
 			</div>
+		  </div>
 		</div>
-
-		<!-- hero area -->
-								<div class="hero-area hero-bg">
-
-								  <div class="container">
-								    <div class="row">
-								      <div class="col-lg-9 offset-lg-2 text-center">
-								        <div class="hero-text">
-								          <div class="hero-text-tablecell">
-								              <img src="@/assets/kira/img/images/s.jpg" alt=""  width= 50%>
-								            <p class="subtitle">Fresh & Organic</p>
-								            <h1>Delicious Nuts</h1>
-								              <div class="hero-btns">
-								              <a href="shop.html" class="boxed-btn">Nuts Collection</a>
-								              <a href="contact.html" class="bordered-btn">Contact Us</a>
-
-								            </div>
-								          </div>
-								        </div>
-								      </div>
-								    </div>
-								  </div>
-								</div>
-								<!-- end hero area -->
+		<!-- end hero area -->
 
 </template>
